6250bda6b8e2a4aec844d3947200791659f21045
EJ Fox <ejfox@ejfox.com>
2014-05-05 14:57:02 -0400

    Adding basic bar chart with labels and axes

--- js/visualizations.coffee
+++ js/visualizations.coffee
@@ -35,7 +35,12 @@ console.log "Section 3"
 
 Tabletop.init(
   key: 'https://docs.google.com/spreadsheets/d/1MzVz_KTvgmxcuC11Z0UIPXYyqb418-6jRjHdPUKZnDo/pubhtml'
-  callback: (data, tabletop) -> 
+  callback: (data, tabletop) ->
+    _.each(data, (row) ->
+      donation = row.donations.substring(1)
+      donation = donation.replace(/,/g , "")
+      row.donations = parseInt(donation)
+    )
     makeBarChart(data)
   simpleSheet: true  
 )
@@ -43,39 +48,89 @@ Tabletop.init(
 makeBarChart = (data) ->
   console.log "Making a bar chart of ", data
 
-  w = 600
-  h = 400
-  margin = 50
+  w = 800
+  h = 600
+  margin = {
+    left: 32
+    top: 32
+    bottom: 32
+    right: 32
+  }
+
+  console.log 'margin', margin, margin.top
+
+  data = _.filter(data, (row) ->
+    row.donations isnt 0
+  )
 
   max = d3.max(data, (d) -> 
-    console.log d.donations, parseInt(d.donations.substring(1))
-    parseInt(d.donations.substring(1))
+    #console.log d.donations.substring(1), parseInt(d.donations.substring(1))
+    d.donations
   )
 
   y = d3.scale.ordinal()
     .domain(d3.range(data.length))
-    .rangeBands([0, h-margin], 0.2)
+    .rangeBands([0, h-( margin.top + margin.bottom )], 0.2)
 
   x = d3.scale.linear()
     .domain([0, max])
-    .range([0,w-margin])
+    .range([0,w-( margin.left + margin.right )])
+
+  xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom')
+    .tickFormat((d) -> 
+      moneyTickFormat = d3.format(".2s")
+      '$' + moneyTickFormat(d)
+    )
 
   svg = d3.select('#section3')
   .append('svg')
   .attr('width', w)
   .attr('height', h)
-  .append('g')
-  .attr("transform", "translate("+margin+","+margin+")")
+  
+  chart = svg.append('g')
+  .attr("transform", "translate("+margin.left+","+margin.top+")")
 
-  bars = svg.selectAll('rect')
+  bars = chart.selectAll('g.rect')
   .data(data)
   .enter()
-  .append('rect')
+  .append('g')
+  .attr('class', 'rect')
+
+  bars.append('rect')
   .attr({
     width: (d,i) ->
-      console.log 'd', d, 'd.donations', parseInt(d.donations.substring(1)), 'x', x(parseInt(d.donations.substring(1)))
-      x(parseInt(d.donations.substring(1)))
+      #console.log 'd', d, 'd.donations', parseInt(d.donations.substring(1)), 'x', x(parseInt(d.donations.substring(1)))
+      x(parseInt(d.donations))
     height: y.rangeBand()
     y: (d,i) ->
-      y(i)
+      parseInt(y(i))
   })
+
+  svg.append("g")
+    .attr('class', 'x axis')
+    .attr('transform', 'translate('+margin.left+','+(h-margin.bottom)+')')
+    .call(xAxis)
+
+
+  labelFontSize = 12
+
+  labels = bars.append("text")
+    .text((d) -> d.state)
+    .style({
+      'text-anchor': 'end'
+      'font-size': labelFontSize+'px'
+    })
+    .attr({
+      x: -5
+      y: (d,i) -> parseInt(y(i)) + labelFontSize
+    })
+
+
+
+
+
+
+
+
--- js/visualizations.js
+++ js/visualizations.js
@@ -51,33 +51,68 @@ console.log("Section 3");
 Tabletop.init({
   key: 'https://docs.google.com/spreadsheets/d/1MzVz_KTvgmxcuC11Z0UIPXYyqb418-6jRjHdPUKZnDo/pubhtml',
   callback: function(data, tabletop) {
+    _.each(data, function(row) {
+      var donation;
+
+      donation = row.donations.substring(1);
+      donation = donation.replace(/,/g, "");
+      return row.donations = parseInt(donation);
+    });
     return makeBarChart(data);
   },
   simpleSheet: true
 });
 
 makeBarChart = function(data) {
-  var bars, h, margin, max, svg, w, x, y;
+  var bars, chart, h, labelFontSize, labels, margin, max, svg, w, x, xAxis, y;
 
   console.log("Making a bar chart of ", data);
-  w = 600;
-  h = 400;
-  margin = 50;
-  max = d3.max(data, function(d) {
-    console.log(d.donations, parseInt(d.donations.substring(1)));
-    return parseInt(d.donations.substring(1));
+  w = 800;
+  h = 600;
+  margin = {
+    left: 32,
+    top: 32,
+    bottom: 32,
+    right: 32
+  };
+  console.log('margin', margin, margin.top);
+  data = _.filter(data, function(row) {
+    return row.donations !== 0;
   });
-  y = d3.scale.ordinal().domain(d3.range(data.length)).rangeBands([0, h - margin], 0.2);
-  x = d3.scale.linear().domain([0, max]).range([0, w - margin]);
-  svg = d3.select('#section3').append('svg').attr('width', w).attr('height', h).append('g').attr("transform", "translate(" + margin + "," + margin + ")");
-  return bars = svg.selectAll('rect').data(data).enter().append('rect').attr({
+  max = d3.max(data, function(d) {
+    return d.donations;
+  });
+  y = d3.scale.ordinal().domain(d3.range(data.length)).rangeBands([0, h - (margin.top + margin.bottom)], 0.2);
+  x = d3.scale.linear().domain([0, max]).range([0, w - (margin.left + margin.right)]);
+  xAxis = d3.svg.axis().scale(x).orient('bottom').tickFormat(function(d) {
+    var moneyTickFormat;
+
+    moneyTickFormat = d3.format(".2s");
+    return '$' + moneyTickFormat(d);
+  });
+  svg = d3.select('#section3').append('svg').attr('width', w).attr('height', h);
+  chart = svg.append('g').attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  bars = chart.selectAll('g.rect').data(data).enter().append('g').attr('class', 'rect');
+  bars.append('rect').attr({
     width: function(d, i) {
-      console.log('d', d, 'd.donations', parseInt(d.donations.substring(1)), 'x', x(parseInt(d.donations.substring(1))));
-      return x(parseInt(d.donations.substring(1)));
+      return x(parseInt(d.donations));
     },
     height: y.rangeBand(),
     y: function(d, i) {
-      return y(i);
+      return parseInt(y(i));
+    }
+  });
+  svg.append("g").attr('class', 'x axis').attr('transform', 'translate(' + margin.left + ',' + (h - margin.bottom) + ')').call(xAxis);
+  labelFontSize = 12;
+  return labels = bars.append("text").text(function(d) {
+    return d.state;
+  }).style({
+    'text-anchor': 'end',
+    'font-size': labelFontSize + 'px'
+  }).attr({
+    x: -5,
+    y: function(d, i) {
+      return parseInt(y(i)) + labelFontSize;
     }
   });
 };
